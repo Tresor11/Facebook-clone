258b95e00b8f285b9c0635d3b49399ba626bb719
Tresor11 <tresor.moise2001@gmail.com>
2020-01-26 19:07:41 +0200

    filled the layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def show; end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.user_name, class: 'gravatar')
+  def my_posts
+    current_user.posts.all
   end
 end
