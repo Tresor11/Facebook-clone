e344961d2ac03361b309935ec3fcfeda28940183
Sharmarke <somaliwarsan@gmail.com>
2020-01-27 22:11:44 +0300

    added collection partial for recent comments, resolved issue where could not post a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @comment=current_user.likes.build(comment_params)
+    @comment = current_user.comments.build(comment_params)
     if @comment.save
       redirect_to @comment.post
     else
@@ -11,6 +11,6 @@ class CommentsController < ApplicationController
   private
   
   def comment_params
-    params.require(:comment).permit(:comment_content,:commenter_id,:post_id)
+    params.require(:comment).permit(:comment_content, :post_id)
   end
 end
