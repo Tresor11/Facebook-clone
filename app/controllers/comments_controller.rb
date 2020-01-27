204c0c226903d2965c39b390a9bdb4de39565277
Tresor11 <tresor.moise2001@gmail.com>
2020-01-27 19:51:35 +0200

    added the comment and like controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    @comment=current_user.likes.build(comment_params)
+    if @comment.save
+      redirect_to @comment.post
+    else
+      redirect_to posts_path
+    end
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:comment_content,:commenter_id,:post_id)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,11 @@
 class LikesController < ApplicationController
+  def created
+    @like=current_user.likes.build(like_params)
+  end
+
+  private
+  
+  def like_params
+    params.require(:like).permit(:user_id,:post_id)
+  end
 end
