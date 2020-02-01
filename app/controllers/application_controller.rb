99270da7a33f63fe864c4e9c22fdca8bc9431425
Tresor11 <tresor.moise2001@gmail.com>
2020-02-01 09:42:36 +0200

    added the unit test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,35 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     posts_url
   end
+
+  def friends
+    sent = sent_requests.map do |friendship|
+      friendship.recever if friendship.status
+    end
+    receved = receved_requests.map do |friendship|
+      friendship.sender if friendship.status
+    end
+    (sent + receved).compact
+end
+
+  def pendeing_request
+    sent_requests.map { |friendship| friendship.recever unless friendship.status }.compact
+  end
+
+  def friend_request
+    receved_requests.map do |friendship|
+      friendship.sender unless friendship.status
+    end .compact
+  end
+
+  def accept_request(user)
+    friendship = receved_requests.find { |f| f.sender == user }
+    friendship.status = true
+    friends << friendship
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,34 +16,34 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: { in: 3..100 }
   validates :gender, presence: true
 
-  def friends
-    sent = sent_requests.map do |friendship|
-      friendship.recever if friendship.status
-    end
-    receved = receved_requests.map do |friendship|
-      friendship.sender if friendship.status
-    end
-    (sent + receved).compact
-  end
+  # def friends
+  #   sent = sent_requests.map do |friendship|
+  #     friendship.recever if friendship.status
+  #   end
+  #   receved = receved_requests.map do |friendship|
+  #     friendship.sender if friendship.status
+  #   end
+  #   (sent + receved).compact
+  # end
 
-  def pendeing_request
-    sent_requests.map { |friendship| friendship.recever unless friendship.status }.compact
-  end
+  # def pendeing_request
+  #   sent_requests.map { |friendship| friendship.recever unless friendship.status }.compact
+  # end
 
-  def friend_request
-    receved_requests.map do |friendship|
-      friendship.sender unless friendship.status
-    end .compact
-  end
+  # def friend_request
+  #   receved_requests.map do |friendship|
+  #     friendship.sender unless friendship.status
+  #   end .compact
+  # end
 
-  def accept_request(user)
-    friendship = receved_requests.find { |f| f.sender == user }
-    friendship.status = true
-    friends << friendship
-    friendship.save
-  end
+  # def accept_request(user)
+  #   friendship = receved_requests.find { |f| f.sender == user }
+  #   friendship.status = true
+  #   friends << friendship
+  #   friendship.save
+  # end
 
-  def friend?(user)
-    friends.include?(user)
-  end
+  # def friend?(user)
+  #   friends.include?(user)
+  # end
 end
