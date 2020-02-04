83f6e71c10aaa75bda9265863287f9b8671b0c84
Sharmarke <somaliwarsan@gmail.com>
2020-02-04 21:57:44 +0300

    added omniauth-facebook gem with omniauth-callbacks for facebook login redirects

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "2560319290923572", "f017d2ec2100755a06ed8153345d378f", token_params: { parse: :json }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- devise_for :users
+ devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
  resources :requests
  resources :posts do
   resources :comments
